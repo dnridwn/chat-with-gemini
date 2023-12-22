464c781553c8e0969f69fbfe7181828b61289546
dnridwn <denridwan123@gmail.com>
2023-12-23 02:44:48 +0700

    seperate middleware logic to middleware.go file

--- backend/main.go
+++ backend/main.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"fmt"
 	"log"
-	"net/http"
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
@@ -24,33 +22,11 @@ func loadENV() {
 
 func route() *gin.Engine {
 	r := gin.New()
-	r.Use(func(c *gin.Context) {
-		defer func() {
-			if err := recover(); err != nil {
-				fmt.Println(fmt.Sprintf("Panic: %v", err))
-				c.JSON(http.StatusInternalServerError, gin.H{
-					"status":  false,
-					"message": "Internal Server Error",
-				})
-			}
-		}()
 
-		c.Next()
-	})
-	r.Use(func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
-		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
-		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
-
-		if c.Request.Method == "OPTIONS" {
-			c.AbortWithStatus(204)
-			return
-		}
-
-		c.Next()
-	})
+	r.Use(panicHandlerMiddleware)
+	r.Use(corsMiddleware)
 	r.Use(gin.Logger())
+
 	r.POST("ask", AskHandler)
 
 	return r
---
+++ backend/middleware.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func panicHandlerMiddleware(c *gin.Context) {
+	defer func() {
+		if err := recover(); err != nil {
+			fmt.Println(fmt.Sprintf("Panic: %v", err))
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"status":  false,
+				"message": "Internal Server Error",
+			})
+		}
+	}()
+
+	c.Next()
+}
+
+func corsMiddleware(c *gin.Context) {
+	c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+	c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+	c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+	c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+	if c.Request.Method == "OPTIONS" {
+		c.AbortWithStatus(204)
+		return
+	}
+
+	c.Next()
+}
